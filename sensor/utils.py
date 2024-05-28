b55476de53ba056679b31435dde1b2ba5115f7f4
yashsuri99 <yashsuri1999@gmail.com>
2024-05-28 16:02:34 +0530

    create files that would be needed in the project connected mongodb and have setup config for the training pipeline

---
+++ sensor/utils.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import json
+from sensor.config import mongo_client
+import logging
+
+def dump_csv_file_mongo(file_path:str, database_name:str, collection_name:str)-> None:
+  
+  try:
+    
+    df = pd.read_csv(file_path)
+    df.reset_index(drop=True, inplace=True)
+    
+    json_records = list(json.loads(df.T.to_json()).values())
+    
+    mongo_client[database_name][collection_name].insert_many(json_records)
+    
+  except Exception as e:
+    pass
