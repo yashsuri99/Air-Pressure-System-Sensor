d4755ff7ded29bca43572854755580f160d418a2
yashsuri99 <yashsuri1999@gmail.com>
2024-05-29 14:24:16 +0530

    Data Ingestion Completed Along with Pipeline Setup when run will get artifact folder which fetches data from mongodb and stores in feature store and splits the data as well into train and test data

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-import sys
+import sys, os
 from sensor.exception import SensorException
 from sensor.logger import logging
-from sensor.utils import dump_csv_file_mongo
+# from sensor.utils import dump_csv_file_mongo
+from sensor.pipeline.training_pipeline import TrainingPipeline
 
-# def test_exception():
-#   try:
-#     logging.info("failed case")
-#     a = 1/0
-#   except Exception as e:
-#     logging.info("error occurred")
-#     raise SensorException(e,sys)
-
-
+def main():
+    try:
+        training_pipeline = TrainingPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        SensorException(e,sys)
 
 if __name__ == "__main__":
-  # try:
-  #   test_exception()
-  # except Exception as e:
-  #   print(e)
   
-  file_path = "aps_failure_training_set1.csv"
-  database_name = "air_pressure_system"
-  collection_name= "sensor"
+  # file_path = "aps_failure_training_set1.csv"
+  # database_name = "air_pressure_system"
+  # collection_name= "sensor"
   
-  dump_csv_file_mongo(file_path, database_name, collection_name)
+  # dump_csv_file_mongo(file_path, database_name, collection_name)
+  main()
